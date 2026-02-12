24d4b9a2c9fc1b80e0923ba5f133e0f9114f7847
Nikita Baruah <nikita_baruah27@milton.edu>
2026-02-12 12:04:00 -0500

    starting the mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://NikitaB03:NikitaB03@cluster0.4ew2jct.mongodb.net/nest-jsdemo')],
   controllers: [AppController],
   providers: [AppService],
 })
